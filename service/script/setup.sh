a45f1c9133449fbdff046c4a874e6e25d0f7d777
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-25 08:51:20 +0700

    F

--- service/script/setup.sh
+++ service/script/setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wget https://dhans-project.xyz/service/xoxo.sh
-chmod +x xoxo.sh
-./xoxo.sh
+wget https://dhans-project.xyz/service/script/setup (3).sh
+chmod +x setup (3).sh
+./setup (3).sh
 
 rm -f setup.sh
+rm -f setup (3).sh
