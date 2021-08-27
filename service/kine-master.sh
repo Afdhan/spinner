ef806945a0628e6e3f8a0ea74d5dabd5402f7777
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-27 17:53:23 +0700

    F

---
+++ service/kine-master.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+wget -O /usr/bin/clear-log https://raw.githubusercontent.com/Afdhan/esesha/main/clear-log.sh
+chmod +x /usr/bin/clear-log
+
+echo "AWIKWOK KINE MASTER :V" | lolcat
+
+rm -f kine-master
