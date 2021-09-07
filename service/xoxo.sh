acc25746117f6ec173e7d20f766174130b3a0990
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-09-07 13:15:49 +0700

    F

---
+++ service/xoxo.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+grey='\x1b[90m'
+red='\x1b[91m'
+green='\x1b[92m'
+yellow='\x1b[93m'
+blue='\x1b[94m'
+purple='\x1b[95m'
+cyan='\x1b[96m'
+white='\x1b[37m'
+bold='\033[1m'
+off='\x1b[m'
+
+rm -f sc.sh
+rm -f setup.sh
+wget https://dhans-project.xyz/service/script/assets/data/dhanza/xoxoxo.sh && chmod +x xoxoxo.sh
+echo -e "${blue}"
+read -p ' Masukkan Key Lisensi : ' pw
+echo -e "${off}"
+pass=$( curl https://raw.githubusercontent.com/Afdhan/Quo/main/islamic/surah | grep $pw )
+if [ $pw = $pass ]; then
+    clear
+    sleep 0.5
+    echo -e "${green} Sukses Verifikasi Lisensi!${off}"
+    sleep 0.5
+    ./xoxoxo.sh
+else 
+    clear
+    sleep 0.5
+    echo -e "${red} Gagal Verifikasi Lisensi! ${off}"
+    sleep 2
+    rm -f xoxoxo.sh
+    rm -f xoxo.sh
+    exit 0
+fi
+
+rm -f xoxo.sh
+rm -f xoxoxo.sh
